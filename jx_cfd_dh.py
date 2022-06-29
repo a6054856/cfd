4fa4c3ba4889fc5a292ecd3f38428b786436e069
院长 <32212075+yuanter@users.noreply.github.com>
2022-06-29 17:35:31 +0800

    长连接和短链接相互兼容

--- jx_cfd_dh.py
+++ jx_cfd_dh.py
@@ -17,11 +17,12 @@ cfd_start_time = -0.15
 cfd_offset_time = 0.01
 
 # 基础配置勿动
-# cfd_url = "https://m.jingxi.com/jxbfd/user/ExchangePrize?strZone=jxbfd&bizCode=jxbfd&source=jxbfd&dwEnv=7&_cfd_t=1648778416388&ptag=7155.9.47&dwType=3&dwLvl=2&ddwPaperMoney=100000&strPoolName=jxcfd2_exchange_hb_202204&strPgtimestamp=1648778416345&strPhoneID=1ef0eafd7b25011a040a37ab925a5586ebfe3f35&strPgUUNum=1674e9a767125864a580ba003b8e655b&_stk=_cfd_t%2CbizCode%2CddwPaperMoney%2CdwEnv%2CdwLvl%2CdwType%2Cptag%2Csource%2CstrPgUUNum%2CstrPgtimestamp%2CstrPhoneID%2CstrPoolName%2CstrZone&_ste=1&h5st=20220401100016388%3B9785369927142102%3B92a36%3Btk02w5c951a1218nwt7hCp33UXI2Fjh60u3JfB%2BJ8MEWiMV%2Fibq4jLJPa3oZjNWJAHB1M35JEoTGURFBUSdW9vmWI3Y4%3B62cd48fa4161a161d28b0491ea99225665b121721a11208c5ffe19b0cf1cf4c7%3B3.0%3B1648778416388&_=1648778416389&sceneval=2&g_login_type=1&callback=jsonpCBKX&g_ty=ls"
+#cfd_url = "https://m.jingxi.com/jxbfd/user/ExchangePrize?strZone=jxbfd&bizCode=jxbfd&source=jxbfd&dwEnv=7&_cfd_t=1655280000000&dwType=3&dwLvl=15&ddwPaperMoney=100000&strPoolName=jxcfd2_exchange_hb_202205&sceneval=2&g_login_type=1"
 #cfd_url = "https://m.jingxi.com/jxbfd/user/ExchangePrize?strZone=jxbfd&bizCode=jxbfd&source=jxbfd&dwEnv=7&_cfd_t=1655382598869&ptag=7155.9.47&dwType=3&dwLvl=15&ddwPaperMoney=100000&strPoolName=jxcfd2_exchange_hb_202205&strPgtimestamp=1655382598856&strPhoneID=057b3d2437f4f6ce&strPgUUNum=8f06503505d4a92b60e4c8523bb22ccc&_stk=_cfd_t%2C_imbfd%2CbizCode%2CddwPaperMoney%2CdwEnv%2CdwLvl%2CdwType%2Cptag%2Csource%2CstrPgUUNum%2CstrPgtimestamp%2CstrPhoneID%2CstrPoolName%2CstrZone&_ste=1&h5st=20220616202958869%3B4770080942685051%3B92a36%3Btk02w41d61a4e18nU1V98c6l0G4APi061CSkjxsMIEgITOn%2Bu%2B8EIBSB2veGTb7RPZY21LZOAnZgS53vqJiiDlrBMEsQ%3B4fb37e267dbe8abd7f2f78dbbecd26d5af4ee1fb0d525186efb85e4078f1dbb7%3B3.1%3B1655382598869%3B62f4d401ae05799f14989d31956d3c5fbb719f372d9ee8d43c5314e243efe804b557706e6a3a6b7efe23ed64146c47c2e5d7e3402135354ed2112c124043bf1fb32a65e8d0c89bd90a2aaefe830f7d4fd883e36ed3133cdc54cf62b77c212a436bd311403abb505cb965e93d4f5747b0&_=1655382598874&sceneval=2&g_login_type=1&callback=jsonpCBKR&g_ty=ls&appCode=msd1188198"
 cfd_url = "https://m.jingxi.com/jxbfd/user/ExchangePrize?strZone=jxbfd&bizCode=jxbfd&source=jxbfd&strDeviceId=49cce8b27ec64014&dwEnv=7&_cfd_t=1656233059204&ptag=138631.77.28&dwType=3&dwLvl=15&ddwPaperMoney=100000&strPoolName=jxcfd2_exchange_hb_202205&strPgtimestamp=1656233059166&strPhoneID=49cce8b27ec64014&strPgUUNum=6be668a8ea88c32c2c4b1aa7a382818d&_stk=_cfd_t%2CbizCode%2CddwPaperMoney%2CdwEnv%2CdwLvl%2CdwType%2Cptag%2Csource%2CstrDeviceId%2CstrPgUUNum%2CstrPgtimestamp%2CstrPhoneID%2CstrPoolName%2CstrZone&_ste=1&h5st=20220626164419213%3B2669750832661957%3B92a36%3Btk02wa9cb1b9c18nUtzeTjeNqXTNK8KqGUp67bRvdUvk4A%2FhUm5%2F8ZblIICCXMK0sRPeqj2%2BsLj1yi%2Bf0s2kS5iYFx%2BP%3B0ea8b6dd76382b3c9dcb451b94cd48483f555356169f521bb102b671844c9767%3B3.1%3B1656233059213%3B62f4d401ae05799f14989d31956d3c5f27ba522a81bb61f6036f822a201e8a4666ed791516731c57f8679a0931698abb493795ffab4e6c26d3026ccd587ba0c9a0b358718bab4f6f16a4bbb97d0e9da90fdb86ed0582385c9f4ec6be04b625c15bed55b4380d88cd6cdcef43d05834d3fdaaa256ed98741738092c698e688e5371df6d09b231cd449788684c57f0b343&_=1656233059250&sceneval=2&g_login_type=1&callback=jsonpCBKM&g_ty=ls&appCode=msd1188198"
 pattern_pin = re.compile(r'pt_pin=([\w\W]*?);')
 pattern_data = re.compile(r'\(([\w\W]*?)\)')
+pattern_jsonp = re.compile(r'jsonpCBKM\(([\w\W]*?)\)')
 
 # 判断新旧版青龙
 ql_auth_path = '/ql/data/config/auth.json'
@@ -93,9 +94,13 @@ def cfd_qq(def_start_time):
     print(res.text)
     t2 = time.time()
     # 正则对结果进行提取
+    re_jsonp = pattern_jsonp.search(res.text)
     re_list = pattern_data.search(res.text)
     # 进行json转换
-    data = json.loads(re_list.group(1))
+    if re_jsonp is None:
+        data = json.loads(res.text)
+    else:
+        data = json.loads(re_list.group(1))
     msg = data['sErrMsg']
     # 根据返回值判断
     if data['iRet'] == 0:
@@ -107,7 +112,7 @@ def cfd_qq(def_start_time):
         elif flag == "new":
             put_envs(u_cookie.get('id'), u_cookie.get('name'), u_cookie.get('value'), msg)
             disable_env(u_cookie.get('id')) 
-        send('财富岛抢购通知', '可能抢到了')       
+        send('财富岛抢购通知', '账号：'+u_pin+'可能抢到了')       
     elif data['iRet'] == 2016:
         # 需要减
         start_time = float(u_start_time) - float(cfd_offset_time)
@@ -134,7 +139,7 @@ def cfd_qq(def_start_time):
         elif flag == "new":
             put_envs(u_cookie.get('id'), u_cookie.get('name'), u_cookie.get('value'), msg)
             disable_env(u_cookie.get('id'))
-        send('财富岛抢购通知', '财富值不够')
+        send('财富岛抢购通知', '账号：'+u_pin+'财富值不够')
     elif data['iRet'] == 9999:
         # 账号过期
         if flag == "old":
@@ -143,7 +148,7 @@ def cfd_qq(def_start_time):
         elif flag == "new":
             put_envs(u_cookie.get('id'), u_cookie.get('name'), u_cookie.get('value'), msg)
             disable_env(u_cookie.get('id'))
-        send('财富岛抢购通知', '抢购账号失效')
+        send('财富岛抢购通知', '账号：'+u_pin+'抢购账号失效')
     print("实际发送[{}]\n耗时[{:.3f}]\n用户[{}]\n结果[{}]".format(d1, (t2 - t1), u_pin, msg))
 
 
